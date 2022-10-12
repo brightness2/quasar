65cb104cd3f7faa66794f4f5efe1314d2f0e8349
brightness2 <2390408895@qq.com>
2022-10-12 18:05:26 +0800

    demo

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Quasar demo
 Quasar demo
+
+# 环境要求
+Node > 14
+npm >  6.13.4
+
+# 环境部署
+```
+npm i -g @quasar/cli
+
+npm init quasar
+```
---
+++ demo/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+console.log(import.meta.env);
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'App'
+})
+</script>
